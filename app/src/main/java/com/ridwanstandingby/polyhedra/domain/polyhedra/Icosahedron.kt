b641cca81119640883af9cebb23b0038caced531
Ridwan Farouki <ridwan@simprints.com>
2019-05-17 05:05:10 +0100

    Fixed variable names

--- app/src/main/java/com/ridwanstandingby/polyhedra/domain/polyhedra/Icosahedron.kt
+++ app/src/main/java/com/ridwanstandingby/polyhedra/domain/polyhedra/Icosahedron.kt
@@ -18,53 +18,53 @@ class Icosahedron(
     private val g = a * (sqrt(5.0) + 1.0) / 2.0
 
     private var opg: FloatVector2 = FloatVector2(0f, 0f)
-    private var oph: FloatVector2 = FloatVector2(0f, 0f)
     private var omg: FloatVector2 = FloatVector2(0f, 0f)
+    private var oph: FloatVector2 = FloatVector2(0f, 0f)
     private var omh: FloatVector2 = FloatVector2(0f, 0f)
     private var gop: FloatVector2 = FloatVector2(0f, 0f)
-    private var hop: FloatVector2 = FloatVector2(0f, 0f)
     private var gom: FloatVector2 = FloatVector2(0f, 0f)
+    private var hop: FloatVector2 = FloatVector2(0f, 0f)
     private var hom: FloatVector2 = FloatVector2(0f, 0f)
     private var pgo: FloatVector2 = FloatVector2(0f, 0f)
-    private var pho: FloatVector2 = FloatVector2(0f, 0f)
     private var mgo: FloatVector2 = FloatVector2(0f, 0f)
+    private var pho: FloatVector2 = FloatVector2(0f, 0f)
     private var mho: FloatVector2 = FloatVector2(0f, 0f)
 
     override fun vertices() =
-        listOf(opg, oph, omg, omh, gop, hop, gom, hom, pgo, pho, mgo, mho)
+        listOf(opg, omg, oph, omh, gop, gom, hop, hom, pgo, mgo, pho, mho)
 
     override fun prepareRender(renderer: PolyhedraAnimationRenderer) {
         val transform = Quaternion(rotation, orientation)
         opg = renderer.camera.project(position + Vector3(0.0, a, g).rotate(transform))
-        oph = renderer.camera.project(position + Vector3(0.0, -a, g).rotate(transform))
-        omg = renderer.camera.project(position + Vector3(0.0, a, -g).rotate(transform))
+        omg = renderer.camera.project(position + Vector3(0.0, -a, g).rotate(transform))
+        oph = renderer.camera.project(position + Vector3(0.0, a, -g).rotate(transform))
         omh = renderer.camera.project(position + Vector3(0.0, -a, -g).rotate(transform))
         gop = renderer.camera.project(position + Vector3(g, 0.0, a).rotate(transform))
-        hop = renderer.camera.project(position + Vector3(g, 0.0, -a).rotate(transform))
-        gom = renderer.camera.project(position + Vector3(-g, 0.0, a).rotate(transform))
+        gom = renderer.camera.project(position + Vector3(g, 0.0, -a).rotate(transform))
+        hop = renderer.camera.project(position + Vector3(-g, 0.0, a).rotate(transform))
         hom = renderer.camera.project(position + Vector3(-g, 0.0, -a).rotate(transform))
         pgo = renderer.camera.project(position + Vector3(a, g, 0.0).rotate(transform))
-        pho = renderer.camera.project(position + Vector3(-a, g, 0.0).rotate(transform))
-        mgo = renderer.camera.project(position + Vector3(a, -g, 0.0).rotate(transform))
+        mgo = renderer.camera.project(position + Vector3(-a, g, 0.0).rotate(transform))
+        pho = renderer.camera.project(position + Vector3(a, -g, 0.0).rotate(transform))
         mho = renderer.camera.project(position + Vector3(-a, -g, 0.0).rotate(transform))
 
         renderer.lines.add(
             floatArrayOf(
-                opg.x, opg.y, pgo.x, pgo.y, pgo.x, pgo.y, hop.x, hop.y,
-                hop.x, hop.y, mgo.x, mgo.y, mgo.x, mgo.y, oph.x, oph.y,
-                oph.x, oph.y, opg.x, opg.y, gop.x, gop.y, opg.x, opg.y,
-                gop.x, gop.y, pgo.x, pgo.y, gop.x, gop.y, hop.x, hop.y,
-                gop.x, gop.y, mgo.x, mgo.y, gop.x, gop.y, oph.x, oph.y,
-                omh.x, omh.y, mho.x, mho.y, mho.x, mho.y, gom.x, gom.y,
+                opg.x, opg.y, pgo.x, pgo.y, pgo.x, pgo.y, gom.x, gom.y,
                 gom.x, gom.y, pho.x, pho.y, pho.x, pho.y, omg.x, omg.y,
-                omg.x, omg.y, omh.x, omh.y, hom.x, hom.y, omh.x, omh.y,
-                hom.x, hom.y, mho.x, mho.y, hom.x, hom.y, gom.x, gom.y,
-                hom.x, hom.y, pho.x, pho.y, hom.x, hom.y, omg.x, omg.y,
-                opg.x, opg.y, pho.x, pho.y, pho.x, pho.y, pgo.x, pgo.y,
-                pgo.x, pgo.y, omg.x, omg.y, omg.x, omg.y, hop.x, hop.y,
-                hop.x, hop.y, omh.x, omh.y, omh.x, omh.y, mgo.x, mgo.y,
-                mgo.x, mgo.y, mho.x, mho.y, mho.x, mho.y, oph.x, oph.y,
-                oph.x, oph.y, gom.x, gom.y, gom.x, gom.y, opg.x, opg.y
+                omg.x, omg.y, opg.x, opg.y, gop.x, gop.y, opg.x, opg.y,
+                gop.x, gop.y, pgo.x, pgo.y, gop.x, gop.y, gom.x, gom.y,
+                gop.x, gop.y, pho.x, pho.y, gop.x, gop.y, omg.x, omg.y,
+                omh.x, omh.y, mho.x, mho.y, mho.x, mho.y, hop.x, hop.y,
+                hop.x, hop.y, mgo.x, mgo.y, mgo.x, mgo.y, oph.x, oph.y,
+                oph.x, oph.y, omh.x, omh.y, hom.x, hom.y, omh.x, omh.y,
+                hom.x, hom.y, mho.x, mho.y, hom.x, hom.y, hop.x, hop.y,
+                hom.x, hom.y, mgo.x, mgo.y, hom.x, hom.y, oph.x, oph.y,
+                opg.x, opg.y, mgo.x, mgo.y, mgo.x, mgo.y, pgo.x, pgo.y,
+                pgo.x, pgo.y, oph.x, oph.y, oph.x, oph.y, gom.x, gom.y,
+                gom.x, gom.y, omh.x, omh.y, omh.x, omh.y, pho.x, pho.y,
+                pho.x, pho.y, mho.x, mho.y, mho.x, mho.y, omg.x, omg.y,
+                omg.x, omg.y, hop.x, hop.y, hop.x, hop.y, opg.x, opg.y
             )
         )
     }
